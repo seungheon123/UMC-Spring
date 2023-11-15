760b5f290190aade16c8939b944c567567dca27a
seungheon123 <seungheon.bisb.2012@gmail.com>
2023-11-15 22:32:52 +0900

    initial setting

---
+++ src/test/java/com/example/umc/UmcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.umc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UmcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
