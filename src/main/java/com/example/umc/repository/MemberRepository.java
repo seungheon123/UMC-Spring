0b9993df435ba226479a75339ebff81255d57dbc
seungheon123 <seungheon.bisb.2012@gmail.com>
2023-11-29 10:34:56 +0900

    Add: MemberRepository

---
+++ src/main/java/com/example/umc/repository/MemberRepository.java
@@ -0,0 +1,7 @@
+package com.example.umc.repository;
+
+import com.example.umc.domain.Member;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MemberRepository extends JpaRepository<Member, Long> {
+}
