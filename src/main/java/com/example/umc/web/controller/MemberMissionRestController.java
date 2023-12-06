d259c8742fbcb5d4689cbf4ed65c9e4136793677
seungheon123 <seungheon.bisb.2012@gmail.com>
2023-12-06 15:46:27 +0900

    Update: PathVariable 수정

--- src/main/java/com/example/umc/web/controller/MemberMissionRestController.java
+++ src/main/java/com/example/umc/web/controller/MemberMissionRestController.java
@@ -3,20 +3,17 @@ package com.example.umc.web.controller;
 import com.example.umc.apiPayload.ApiResponse;
 import com.example.umc.converter.MemberMissionConverter;
 import com.example.umc.domain.mapping.MemberMission;
-import com.example.umc.service.MemberMission.MemberMissionCommandService;
+import com.example.umc.service.MemberMissionService.MemberMissionCommandService;
 import com.example.umc.validation.annotation.ExistMember;
 import com.example.umc.validation.annotation.ExistMission;
 import com.example.umc.validation.annotation.VerifiedMissionChallenge;
-import com.example.umc.web.dto.MemberMissionRequestDto;
 import com.example.umc.web.dto.MemberMissionResponseDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-
-import javax.validation.Valid;
 
 
 @RestController
@@ -28,9 +25,9 @@ public class MemberMissionRestController {
 
     @PostMapping("/member/{memberId}/mission/{missionId}")
     public ApiResponse<MemberMissionResponseDto.MemberMissionResultDto> register(
-            @ExistMember @RequestParam("memberId") Long memberId,
-            @ExistMission @RequestParam("missionId") Long missionId
-    ){
+            @ExistMember @PathVariable Long memberId,
+            @ExistMission @PathVariable Long missionId){
+        System.out.println(missionId);
         MemberMission memberMission = memberMissionCommandService.registerMemberMission(memberId, missionId);
         return ApiResponse.onSuccess(MemberMissionConverter.toMemberMissionResultDto(memberMission));
     }
