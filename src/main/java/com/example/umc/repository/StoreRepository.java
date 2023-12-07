f1b27b6bd9bea32119baea7b89cddd3bfab5bd5a
seungheon123 <seungheon.bisb.2012@gmail.com>
2023-12-07 13:41:46 +0900

    Update: 특정 가게 미션 목록 조회 API

--- src/main/java/com/example/umc/repository/StoreRepository.java
+++ src/main/java/com/example/umc/repository/StoreRepository.java
@@ -4,4 +4,5 @@ import com.example.umc.domain.Store;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface StoreRepository extends JpaRepository<Store,Long> {
+
 }
--- src/main/java/com/example/umc/service/StoreService/StoreCommandServiceImpl.java
+++ src/main/java/com/example/umc/service/StoreService/StoreCommandServiceImpl.java
@@ -3,12 +3,16 @@ package com.example.umc.service.StoreService;
 import com.example.umc.apiPayload.code.status.ErrorStatus;
 import com.example.umc.apiPayload.exception.handler.RegionHandler;
 import com.example.umc.converter.StoreConverter;
+import com.example.umc.domain.Mission;
 import com.example.umc.domain.Region;
 import com.example.umc.domain.Store;
+import com.example.umc.repository.MissionRepository;
 import com.example.umc.repository.RegionRepository;
 import com.example.umc.repository.StoreRepository;
 import com.example.umc.web.dto.StoreRequestDto;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -18,6 +22,7 @@ import org.springframework.transaction.annotation.Transactional;
 public class StoreCommandServiceImpl implements StoreCommandService {
 
     private final StoreRepository storeRepository;
+    private final MissionRepository missionRepository;
     private final RegionRepository regionRepository;
     @Override
     @Transactional
@@ -31,6 +36,12 @@ public class StoreCommandServiceImpl implements StoreCommandService {
     @Override
     public Boolean existStore(Long storeId){
         return storeRepository.existsById(storeId);
+    }
+
+    @Override
+    public Page<Mission> getMissionList(Long storeId, Integer page) {
+        Store store = storeRepository.findById(storeId).get();
+        return missionRepository.findAllByStore(store, PageRequest.of(page,10));
     }
 
 
