e12bd6cf119ad2730edc67dabdafebce983833ff
seungheon123 <seungheon.bisb.2012@gmail.com>
2023-12-07 13:42:22 +0900

    Update: 진행중인 미션 목록 조회 API

--- src/main/java/com/example/umc/converter/MemberMissionConverter.java
+++ src/main/java/com/example/umc/converter/MemberMissionConverter.java
@@ -5,6 +5,10 @@ import com.example.umc.domain.Mission;
 import com.example.umc.domain.enums.MissionStatus;
 import com.example.umc.domain.mapping.MemberMission;
 import com.example.umc.web.dto.MemberMissionResponseDto;
+import org.springframework.data.domain.Page;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class MemberMissionConverter {
 
@@ -22,5 +26,19 @@ public class MemberMissionConverter {
                 .missionStatus(memberMission.getMissionStatus())
                 .createdAt(memberMission.getCreatedAt())
                 .build();
+    }
+
+    public static MemberMissionResponseDto.MemberMissionPreviewListDto toMemberMissionPreviewListDto(Page<MemberMission> missionList){
+        List<MemberMissionResponseDto.MemberMissionPreviewDto> list = missionList.stream()
+                .map(mission -> MemberMissionResponseDto.MemberMissionPreviewDto.builder()
+                        .id(mission.getId())
+                        .title(mission.getMission().getTitle())
+                        .storeName(mission.getMission().getStore().getName())
+                        .body(mission.getMission().getContent())
+                        .point(mission.getMission().getPoint())
+                        .status(mission.getMissionStatus())
+                        .createdAt(mission.getCreatedAt().toLocalDate()).build())
+                .collect(Collectors.toList());
+        return MemberMissionResponseDto.MemberMissionPreviewListDto.builder().missionList(list).build();
     }
 }
--- src/main/java/com/example/umc/service/MemberMissionService/MemberMissionCommandService.java
+++ src/main/java/com/example/umc/service/MemberMissionService/MemberMissionCommandService.java
@@ -1,8 +1,12 @@
 package com.example.umc.service.MemberMissionService;
 
 import com.example.umc.domain.mapping.MemberMission;
+import org.springframework.data.domain.Page;
 
 public interface MemberMissionCommandService {
     MemberMission registerMemberMission(Long memberId, Long missionId);
     Boolean checkIfChallenging(Long id);
+    Page<MemberMission> getMemberMissionList(Long memberId, Integer page);
+
+    MemberMission updateMemberMissionStatus(Long memberMissionId);
 }
