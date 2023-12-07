835e0a3f6679417a8a21fcaa7c550f9b649e1ffb
seungheon123 <seungheon.bisb.2012@gmail.com>
2023-12-07 13:42:52 +0900

    Update: 내가 작성한 리뷰 목록 조회 API

--- src/main/java/com/example/umc/service/MemberService/MemberCommandService.java
+++ src/main/java/com/example/umc/service/MemberService/MemberCommandService.java
@@ -1,5 +1,10 @@
 package com.example.umc.service.MemberService;
 
+import com.example.umc.domain.mapping.Review;
+import org.springframework.data.domain.Page;
+
 public interface MemberCommandService {
     boolean existMember(Long memberId);
+
+    Page<Review> getReviewList(Long memberId, Integer page);
 }
